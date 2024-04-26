b288a335c4ede23ed7bc62226a249d0cabd4e051
Matthias Brunner <brunner.matthias@outlook.com>
2024-04-26 18:30:06 +0200

    refactor: removed unused framework gulp

--- EFTQuestManager-backend/EFTQuestManager-backend-gateway/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-
-gulp.task('move', function () {
-    return gulp.src('server.js') // Pfade zu den Quelldateien
-        .pipe(gulp.dest('dist')); // Zielverzeichnis
-});
-
-gulp.task('default', gulp.series('move'));
